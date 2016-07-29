a5513a2aad9bcf99d8f9bb3b1c411b0a20d4ac1d
Sounkoun Soumare <vsoumare@yahoo.com>
2016-07-29 15:09:12 +0000

    Create a blog page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/portfolio' => 'pages#portfolio'
   get '/about' => 'pages#about'
   resources :contacts
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729094904) do
+ActiveRecord::Schema.define(version: 20160729120316) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
